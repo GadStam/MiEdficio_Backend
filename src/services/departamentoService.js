de925fd33140fd613bdd5b2f265a1f8b1d94a01e
EITAN DANIEL <46028383@ort>
2022-06-29 08:51:03 -0300

    creacion del codigo

--- src/services/departamentoService.js
+++ src/services/departamentoService.js
@@ -6,26 +6,41 @@ const departamentoTabla = process.env.DB_TABLA_DEPARTAMENTO;
 
 export class DepartamentoService {
 
+    /*generateCode=() => {
+        let codigo= ''
+        for(let m=0; m<1;m++){
+            let char=Math.floor(Math.random() * (36 - 0)) + 0;
+            codigo=codigo + letras[char]
+        }
+
+        return codigo
+    }*/
+
     createDepartamento = async (departamento) => {
         console.log('This is a function on the service');
         let response
-        let codigos
+        //let codigos
         let query
         let k=0;
         let letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "0","1","2","3","4","5","6","7","8","9"]
-        let query2 = `SELECT Codigo FROM ${DB_TABLA_DEPARTAMENTO}`
+        //let query2 = `SELECT Codigo FROM ${departamentoTabla}`
         const {automatico, letra, correlativa, cant_pisos, departamentosXpiso}=departamento
         for(let i=0; i<cant_pisos; i++){
             if(automatico==="true"){
                 for(let j=0;  j<departamentosXpiso; j++){
-                    let codigo
-                    codigo=generateCode()
+                    let codigo= ''
+                    for(let m=0; m<6;m++){
+                        let char=Math.floor(Math.random() * (36 - 0)) + 0;
+                        codigo=codigo + letras[char]
+                    }
+                    /*codigo=DepartamentoService.generateCode()
                     codigos=await dbHelper(undefined,departamento,query2)
+                    console.log(codigos)
                     codigos.forEach(cod => {
                         if (codigo === cod) {
-                            codigo=generateCode()
+                            codigo=DepartamentoService.generateCode()
                         }
-                    })
+                    })*/
                     if(letra==="true"){
                         query=`INSERT INTO ${departamentoTabla} (Codigo, Departamento, Id_Edificio) VALUES ('${codigo}', '${i+1}${letras[j]}', @Id_Edificio) `
                         response=await dbHelper (undefined,departamento,query)
@@ -40,25 +55,30 @@ export class DepartamentoService {
                 }
             }else{
                 for(let j=0; j<departamentosXpiso[i]; j++){
-                    let codigo
-                    codigo=generateCode()
+                    let codigo= ''
+                    for(let m=0; m<6;m++){
+                        let char=Math.floor(Math.random() * (36 - 0)) + 0;
+                        codigo=codigo + letras[char]
+                    }
+                    /*codigo=DepartamentoService.generateCode()
                     codigos=await dbHelper(undefined,departamento,query2)
+                    console.log(codigos)
                     codigos.forEach(cod => {
                         if (codigo === cod) {
-                            codigo=generateCode()
+                            codigo=DepartamentoService.generateCode()
                         }
-                    })
-                }
-                if(letra==="true"){
-                    query=`INSERT INTO ${departamentoTabla} (Codigo, Departamento, Id_Edificio) VALUES ('${codigo}', '${i+1}${letras[j]}', @Id_Edificio) `
-                    response=await dbHelper (undefined,departamento,query)
-                }else if(correlativa==="false"){
-                    query=`INSERT INTO ${departamentoTabla} (Codigo, Departamento, Id_Edificio) VALUES ('${codigo}', '${i+1}.${j+1}', @Id_Edificio) `;
-                    response=await dbHelper (undefined,departamento,query)
-                }else{
-                    k++;
-                    query=`INSERT INTO ${departamentoTabla} (Codigo, Departamento, Id_Edificio) VALUES ('${codigo}', '${i+1}.${k}', @Id_Edificio) `;
-                    response=await dbHelper (undefined,departamento,query)
+                    })*/
+                    if(letra==="true"){
+                        query=`INSERT INTO ${departamentoTabla} (Codigo, Departamento, Id_Edificio) VALUES ('${codigo}', '${i+1}${letras[j]}', @Id_Edificio) `
+                        response=await dbHelper (undefined,departamento,query)
+                    }else if(correlativa==="false"){
+                        query=`INSERT INTO ${departamentoTabla} (Codigo, Departamento, Id_Edificio) VALUES ('${codigo}', '${i+1}.${j+1}', @Id_Edificio) `;
+                        response=await dbHelper (undefined,departamento,query)
+                    }else{
+                        k++;
+                        query=`INSERT INTO ${departamentoTabla} (Codigo, Departamento, Id_Edificio) VALUES ('${codigo}', '${i+1}.${k}', @Id_Edificio) `;
+                        response=await dbHelper (undefined,departamento,query)
+                    }
                 }
             }
         }
@@ -82,15 +102,5 @@ export class DepartamentoService {
         console.log(departamento.codigo)
 
         return response.recordset;
-    }
-
-    generateCode = async() => {
-        let codigo= ''
-        for(let m=0; m<1;m++){
-            let char=Math.floor(Math.random() * (36 - 0)) + 0;
-            codigo=codigo + letras[char]
-        }
-
-        return codigo
     }
 }
