1529501211e7c0fcdb9260091b15d713be342258
EitanFiszer <eitandfiszer@gmail.com>
2022-07-18 15:29:51 -0300

    restart heroku

--- src/services/espacioService.js
+++ src/services/espacioService.js
@@ -13,7 +13,6 @@ export class EspacioService {
         let query=`SELECT * from ${espacioTabla}`
         response=await pool.query(query)
         console.log(response)
-        await pool.end()
         return response.rows;
     }
 
