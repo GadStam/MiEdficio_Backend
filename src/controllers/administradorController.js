381adcc3a5cac935a8ac2992872be3933437ea9f
EitanFiszer <eitandfiszer@gmail.com>
2022-07-18 14:43:44 -0300

    restart heroku

--- src/controllers/administradorController.js
+++ src/controllers/administradorController.js
@@ -84,22 +84,19 @@ router.post('', async(req, res) => {
 
 router.post('/logIn', async(req, res) => {
     console.log(`This is a post operation`);
-    try {
+    //try {
         const administrador = await administradorService.getAdministrador(req.body);
         if (administrador[0] === undefined) {
             return res.status(404).json("Error en el loguearse");
         } else {
-            console.log("1")
             const id = administrador[0].id_administrador
-            console.log("2")
             const token = authService.getToken(id)
-            console.log("3")
             return res.status(200).json({ token, id });
         }
-    }catch (error) {
-        console.log(error)
-        return res.status(502).json(error)
-    }
+    //}catch (error) {
+        //console.log(error)
+        //return res.status(502).json(error)
+    //}
 });
 
 router.get('/:id', Authenticate, async(req, res) => {
