8732caa480418175b4747fdb7d43225ee2585911
GadStam <gadstam71@gmail.com>
2022-07-23 18:45:53 -0300

    refresh

--- src/services/administradorService.js
+++ src/services/administradorService.js
@@ -22,7 +22,7 @@ export class AdministradorService {
         
         return response.rowCount;
     }
-//hodaaaaaaaaaaa
+//hodaaaaaaaaaaaa
     getAdministrador = async(administrador) => {
         console.log('This is a function on the service');
         let response = 0
