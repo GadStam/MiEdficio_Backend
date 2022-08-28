140558a26f5caeb7ff3ae9b81053e037bd31af4a
EitanFiszer <eitandfiszer@gmail.com>
2022-08-28 14:48:36 -0300

    sumar horas

--- src/services/eventoService.js
+++ src/services/eventoService.js
@@ -11,11 +11,30 @@ export class EventoService {
         console.log('This is a function on the service');
         let response
         let response2
-        let hora_final=parseInt(evento.hora_inicio)+evento.horas
+
+        let hora_final=evento.hora_inicio+evento.horas
         console.log(hora_final)
-        const hora_final_string=hora_final + ':00:00'
-        console.log(hora_final_string)
-        const query = `INSERT INTO ${eventoTabla} (fecha, hora_inicio, hora_final, cant_invitados, id_departamento, id_espaciocomun, id_edificio, horas) VALUES ('${evento.fecha}', '${evento.hora_inicio}', '${hora_final_string}', '${evento.cant_invitados}', '${evento.id_departamento}', '${evento.id_espaciocomun}', '${evento.id_edificio}', '${evento.horas}') `;
+
+        let hora_inicio_string=evento.hora_inicio+''
+        let hora_final_string=hora_final + ''
+        hora_final_string=hora_final_string.replace('.',':')
+        hora_inicio_string=hora_inicio_string.replace('.',':')
+        if(hora_inicio_string.length===4){
+            hora_final_string=hora_final_string+'0:00'
+            hora_inicio_string=hora_inicio_string+'0:00'
+        }else if (hora_inicio_string.length===5){
+            hora_final_string=hora_final_string+':00'
+            hora_inicio_string=hora_inicio_string+':00'
+        }else if (hora_inicio_string.length===2){
+            hora_final_string=hora_final_string+':00:00'
+            hora_inicio_string=hora_inicio_string+':00:00'
+        }else if(hora_inicio_string.length===1){
+            hora_final_string='0'+hora_final_string+':00:00'
+            hora_inicio_string='0'+hora_inicio_string+':00:00'
+        }
+        console.log(hora_final_string,hora_inicio_string)
+
+        const query = `INSERT INTO ${eventoTabla} (fecha, hora_inicio, hora_final, cant_invitados, id_departamento, id_espaciocomun, id_edificio, horas) VALUES ('${evento.fecha}', '${hora_inicio_string}', '${hora_final_string}', '${evento.cant_invitados}', '${evento.id_departamento}', '${evento.id_espaciocomun}', '${evento.id_edificio}', '${evento.horas}') `;
         const query2 = `SELECT hora_inicio, hora_final from ${eventoTabla} where id_edificio=${evento.id_edificio} and id_espaciocomun=${evento.id_espaciocomun} and fecha='${evento.fecha}'`
         const { Pool } = pkg;
         const pool = new Pool(
@@ -29,7 +48,7 @@ export class EventoService {
             response2=await pool.query(query2)
             console.log(response2.rows)
 
-        const even = (element) => (evento.hora_inicio >= element.hora_inicio && evento.hora_inicio<element.hora_final) || (hora_final_string>element.hora_inicio && hora_final_string<=element.hora_final) || (evento.hora_inicio < element.hora_inicio && hora_final_string> element.hora_final) ;
+        const even = (element) => (hora_inicio_string >= element.hora_inicio && hora_inicio_string<element.hora_final) || (hora_final_string>element.hora_inicio && hora_final_string<=element.hora_final) || (hora_inicio_string < element.hora_inicio && hora_final_string> element.hora_final) ;
 
         const sePisan=(response2.rows.some(even));
         console.log(sePisan)
