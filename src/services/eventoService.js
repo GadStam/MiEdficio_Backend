f8bfd1964de529d2d0929f3aa1400529945c81b6
EitanFiszer <eitandfiszer@gmail.com>
2022-08-28 12:58:04 -0300

    validar eventos

--- src/services/eventoService.js
+++ src/services/eventoService.js
@@ -11,7 +11,11 @@ export class EventoService {
         console.log('This is a function on the service');
         let response
         let response2
-        const query = `INSERT INTO ${eventoTabla} (fecha, hora_inicio, cant_invitados, id_departamento, id_espaciocomun, id_edificio, horas) VALUES ('${evento.fecha}', '${evento.hora_inicio}', '${evento.cant_invitados}', '${evento.id_departamento}', '${evento.id_espaciocomun}', '${evento.id_edificio}', '${evento.horas}' ) `;
+        let hora_final=parseInt(evento.hora_inicio)+evento.horas
+        console.log(hora_final)
+        const hora_final_string=hora_final + ':00:00'
+        console.log(hora_final_string)
+        const query = `INSERT INTO ${eventoTabla} (fecha, hora_inicio, hora_final, cant_invitados, id_departamento, id_espaciocomun, id_edificio, horas) VALUES ('${evento.fecha}', '${evento.hora_inicio}', '${hora_final_string}', '${evento.cant_invitados}', '${evento.id_departamento}', '${evento.id_espaciocomun}', '${evento.id_edificio}', '${evento.horas}') `;
         const query2 = `SELECT hora_inicio, hora_final from ${eventoTabla} where id_edificio=${evento.id_edificio} and id_espaciocomun=${evento.id_espaciocomun} and fecha='${evento.fecha}'`
         const { Pool } = pkg;
         const pool = new Pool(
@@ -22,22 +26,22 @@ export class EventoService {
                 }
             })
             console.log(query2)
-            //response2=await pool.query(query2)
-            //console.log(response2.rows)
+            response2=await pool.query(query2)
+            console.log(response2.rows)
 
-        //const even = (element) => (evento.hora_inicio >= element.hora_inicio && evento.hora_inicio<element.hora_final) || (evento.hora_final>element.hora_inicio && evento.hora_final<=element.hora_final) || (evento.hora_inicio < element.hora_inicio && evento.hora_final> element.hora_final) ;
+        const even = (element) => (evento.hora_inicio >= element.hora_inicio && evento.hora_inicio<element.hora_final) || (hora_final_string>element.hora_inicio && hora_final_string<=element.hora_final) || (evento.hora_inicio < element.hora_inicio && hora_final_string> element.hora_final) ;
 
-        //const sePisan=(response2.rows.some(even));
-        //console.log(sePisan)
-        //if(sePisan===false){
+        const sePisan=(response2.rows.some(even));
+        console.log(sePisan)
+        if(sePisan===false){
             console.log(query)
             response = await pool.query(query)//crea un espacio
             pool.end()
             return response.rowCount
-        /*}else{
+        }else{
             pool.end()
             return response
-        }*/       
+        }       
     }
 
     getEventosByEdificio = async (id, fecha) => {
